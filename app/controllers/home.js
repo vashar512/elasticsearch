8894da2cf123afd2acc03e3f3b10a84d64666a53
Vishal Ashar <vashar512@gmail.com>
2014-12-16 02:20:16 -0500

    Setup models, views and controllers for the app

---
+++ app/controllers/home.js
@@ -0,0 +1,65 @@
+var express = require('express'),
+  router = express.Router(),
+  Article = require('../models/article');
+
+
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+   host: 'localhost:9200',
+  sniffOnStart: true,
+  sniffInterval: 60000,
+  trace: true
+});
+
+client.ping({
+  requestTimeout: 1000,
+  // undocumented params are appended to the query string
+  hello: "elasticsearch!"
+}, function (error) {
+  if (error) {
+    console.error('Elastic Search is down!');
+  } else {}
+});
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+
+router.get('/', function (req, res, next) {
+  var articles = [new Article(), new Article()];
+    res.render('index', {
+      title: 'ElastySearch',
+      articles: articles
+    });
+  var query = encodeURI(req.body.query);
+});
+
+//getting post query
+router.post('/response', function (req, res) {
+
+  //searching twitter index for query
+    client.search({
+      index:'twitter',
+      type: 'tweets',
+      body: {
+        query: {
+          match: {
+            text: req.body.query
+          }
+        }
+      }
+    }, function (error, response) {
+        results = response;
+        hits = results.hits.hits;
+      if (hits.length > -1){
+        for(var i = 0; i<hits.length; i++) {
+          var result = hits[i];
+        }
+      }
+      res.render('response', { query: req.body.query, results: hits});
+    });
+});
+
+
+
