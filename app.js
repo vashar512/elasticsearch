dabe8d087b7bf78a269f71c25bdcd1d4059a6350
Vishal Ashar <vashar512@gmail.com>
2014-12-16 03:20:57 -0500

    Added fix for app not working

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var express = require('express'),
   config = require('./config/config'),
+  keys = require('./oauth.js'),
   elasticsearch = require('elasticsearch'),
-  keys = require('./oauth.js');
+  CronJob = require('cron').CronJob;
 
 var client = new elasticsearch.Client({
-    host: 'localhost:9200',
-    sniffOnStart: true,
-    sniffInterval: 60000,
-    trace: true
-});
+                    host: 'localhost:9200',
+                    sniffOnStart: true,
+                    sniffInterval: 60000,
+                    trace: true
+                  });
 
 var twitterKey = keys.twitter.consumerKey,
     twitterSecret = keys.twitter.consumerSecret,
@@ -25,36 +26,41 @@ var oauth = new OAuth.OAuth(
   null,
   'HMAC-SHA1'
 );
-
-oauth.get('https://api.twitter.com/1.1/statuses/home_timeline.json',
+new CronJob('15 * * * * *', function(){
+    console.log('cron job is running...');
+    oauth.get('https://api.twitter.com/1.1/statuses/home_timeline.json',
       token,
       secret,
       function (error, data, response){
         if (error) console.error(error);
         json = JSON.parse(data);
         tweets = JSON.stringify(json);
-        
-	for(var i = 0; i<json.length; i++)
-        {
-          var item = json[i];
-          client.create({
-            index: 'twitter',
-            type: 'tweets',
-            id: item.id,
-            body: item
-          }, function (error, response) {
-            if(error) {
-              console.log(error);
-            } else {
-              console.log("index created!");
-            }
-          });
-        }
-      }
-);
+
+        for(var i = 0; i<json.length; i++)
+          {
+            var item = json[i];
+            client.create({
+              index: 'twitter',
+              type: 'tweets',
+              id: item.id,
+              body: item
+            }, function (error, response) {
+              if(error) {
+                console.log(error);
+              } else {
+                console.log("index created!");
+              }
+            });
+          }
+    });
+}, null, true, "America/New_York");
+
 
 var app = express();
 
-//require('./config/express')(app, config);
+require('./config/express')(app, config);
 
 app.listen(config.port);
+
+
+
